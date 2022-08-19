dede93b1bb262008e1bfe4490e5641c6bd275b7b
njmwasmoringa <julius.mwangi@moringaschool.com>
2022-08-19 16:25:43 +0300

    All tests pass

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false);
+  let itemClass = inCart ? 'in-cart' : '';
+  let buttonText = inCart ? 'Remove From Cart' : 'Add to Cart';
+  function handleAddToCart(){
+    setInCart(()=>!inCart);
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>{buttonText}</button>
     </li>
   );
 }
